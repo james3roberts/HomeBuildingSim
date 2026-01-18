be50e7112991c3e72fd8d99c48bddc17cabd544f
James <james3roberts@gmail.com>
2026-01-17 21:06:37 -0700

    finished environment setup

--- environment.py
+++ environment.py
@@ -5,6 +5,8 @@ class Environment:
         self.progress = {
             "floor": 0,
             "walls": 0,
+            "floor2": 0,
+            "walls2": 0,
             "roof": 0,
             "stairs": 0
         }
@@ -90,4 +92,136 @@ class Environment:
         self.history.append(
             f"{agent.username} frame floor"
             f"(+{progress_increment} floor, -{stamina_cost} stamina)"
+        )
+            
+    def layout_walls(self,agent):
+        progress_increment = agent.strength + agent.tool_skills
+        stamina_cost = 5
+
+        self.progress["walls"] += progress_increment
+        if self.progress["walls"] > 100:
+            self.progress["walls"]=100
+
+        agent.stamina -= stamina_cost
+        agent.experience += 3
+
+        self.history.append(
+            f"{agent.username} layout walls"
+            f"(+{progress_increment} walls, -{stamina_cost} stamina)"
+        )
+
+    def frame_walls(self,agent):
+        progress_increment = agent.strength + agent.tool_skills
+        stamina_cost = 5
+
+        self.progress["walls"] += progress_increment
+        if self.progress["walls"] > 100:
+            self.progress["walls"]=100
+
+        agent.stamina -= stamina_cost
+        agent.experience += 3
+
+        self.history.append(
+            f"{agent.username} frame walls"
+            f"(+{progress_increment} walls, -{stamina_cost} stamina)"
+        )
+
+    def layout_floor2(self,agent):
+        progress_increment = agent.strength + agent.tool_skills
+        stamina_cost = 4
+
+        self.progress["floor2"] += progress_increment
+        if self.progress["floor2"] > 100:
+            self.progress["floor2"]=100
+
+        agent.stamina -= stamina_cost
+        agent.experience += 2
+
+        self.history.append(
+            f"{agent.username} layout floor 2"
+            f"(+{progress_increment} floor2, -{stamina_cost} stamina)"
+        )
+    
+
+    def frame_floor2(self,agent):
+        progress_increment = agent.strength + agent.tool_skills
+        stamina_cost = 5
+
+        self.progress["floor2"] += progress_increment
+        if self.progress["floor2"] > 100:
+            self.progress["floor2"]=100
+
+        agent.stamina -= stamina_cost
+        agent.experience += 3
+
+        self.history.append(
+            f"{agent.username} frame floor2"
+            f"(+{progress_increment} floor2, -{stamina_cost} stamina)"
+        )
+    
+
+
+    def layout_stairs(self,agent):
+        progress_increment = agent.strength + agent.tool_skills
+        stamina_cost = 5
+
+        self.progress["stairs"] += progress_increment
+        if self.progress["stairs"] > 100:
+            self.progress["stairs"]=100
+
+        agent.stamina -= stamina_cost
+        agent.experience += 3
+
+        self.history.append(
+            f"{agent.username} layout stairs"
+            f"(+{progress_increment} stairs, -{stamina_cost} stamina)"
+        )
+
+
+    def frame_stairs(self,agent):
+        progress_increment = agent.strength + agent.tool_skills
+        stamina_cost = 5
+
+        self.progress["stairs"] += progress_increment
+        if self.progress["stairs"] > 100:
+            self.progress["stairs"]=100
+
+        agent.stamina -= stamina_cost
+        agent.experience += 3
+
+        self.history.append(
+            f"{agent.username} frame stairs"
+            f"(+{progress_increment} stairs, -{stamina_cost} stamina)"
+        )
+
+    def layout_roof(self,agent):
+        progress_increment = agent.strength + agent.tool_skills
+        stamina_cost = 5
+
+        self.progress["roof"] += progress_increment
+        if self.progress["roof"] > 100:
+            self.progress["roof"]=100
+
+        agent.stamina -= stamina_cost
+        agent.experience += 3
+
+        self.history.append(
+            f"{agent.username} layout roof"
+            f"(+{progress_increment} roof, -{stamina_cost} stamina)"
+        )
+
+    def frame_roof(self,agent):
+        progress_increment = agent.strength + agent.tool_skills
+        stamina_cost = 5
+
+        self.progress["roof"] += progress_increment
+        if self.progress["roof"] > 100:
+            self.progress["roof"]=100
+
+        agent.stamina -= stamina_cost
+        agent.experience += 3
+
+        self.history.append(
+            f"{agent.username} frame roof"
+            f"(+{progress_increment} roof, -{stamina_cost} stamina)"
         )
